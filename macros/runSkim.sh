a0f33828b736aa38677af93cc0000cceca5b01f4
Benjamin <benjaminkreis@gmail.com>
2015-12-22 14:05:48 -0600

    script to run Skim.C for v14 heppy ntuples

---
+++ macros/runSkim.sh
@@ -0,0 +1,29 @@
+#Done
+#root -l -b -q Skim.C+\(\"ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8\"\) 
+#root -l -b -q Skim.C+\(\"WH_HToBB_WToLNu_M125_13TeV_amcatnloFXFX_madspin_pythia8\"\) 
+#root -l -b -q Skim.C+\(\"WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8\"\) 
+#root -l -b -q Skim.C+\(\"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8\"\) 
+#root -l -b -q Skim.C+\(\"Muon\"\) 
+
+#root -l -b -q Skim.C+\(\"TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8\"\) 
+#root -l -b -q Skim.C+\(\"TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8\"\) 
+#root -l -b -q Skim.C+\(\"TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8\"\) 
+
+#root -l -b -q Skim.C+\(\"ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1\"\) 
+#root -l -b -q Skim.C+\(\"ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1\"\) 
+#root -l -b -q Skim.C+\(\"ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1\"\) 
+
+root -l -b -q Skim.C+\(\"ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1\"\) 
+
+#root -l -b -q Skim.C+\(\"WW_TuneCUETP8M1_13TeV-pythia8\"\) 
+#root -l -b -q Skim.C+\(\"WZ_TuneCUETP8M1_13TeV-pythia8\"\) 
+#root -l -b -q Skim.C+\(\"ZZ_TuneCUETP8M1_13TeV-pythia8\"\) 
+
+#root -l -b -q Skim.C+\(\"SingleElectron\"\) 
+#root -l -b -q Skim.C+\(\"MET\"\) 
+#root -l -b -q Skim.C+\(\"DoubleEG\"\) 
+#root -l -b -q Skim.C+\(\"DoubleMuon\"\) 
+
+
+
+#root -l -b -q Skim.C+\(\"\"\) 
