c44a4ef4e6b96465d8d7516fa2bbda722af9b100
Benjamin <benjaminkreis@gmail.com>
2015-12-22 15:01:51 -0600

    other signal xsecs

--- macros/XSec_13TeV1p3invfb.h
+++ macros/XSec_13TeV1p3invfb.h
@@ -9,9 +9,9 @@ std::map<std::string, float> GetLumis() {
     std::map<std::string, float> values;
     // Equivalent lumi = lumi * xsec / counts-with-pu
     // counts-with-pu is ((TH1F*)_file0->Get("CountWeighted"))->GetBinContent(1)
-    values["ZH_HToBB_ZToNuNu_M125_13TeV_amcatnloFXFX_madspin_pythia8"                ] = lumi *         0.0 / 1.16478525000000000e+06;
-    values["WH_HToBB_WToLNu_M125_13TeV_amcatnloFXFX_madspin_pythia8"                 ] = lumi *         0.0 / 1.12889112500000000e+06;
-    values["ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8"                                ] = lumi *         0.04837 / 1.86044725000000000e+06;
+    values["ZH_HToBB_ZToNuNu_M125_13TeV_amcatnloFXFX_madspin_pythia8"                ] = lumi *    0.088154 / 1.16478525000000000e+06; //(0.8696-0.1057)*0.200*0.577
+    values["WH_HToBB_WToLNu_M125_13TeV_amcatnloFXFX_madspin_pythia8"                 ] = lumi *     0.25958 / 1.12889112500000000e+06; //1.380 *0.326*0.577
+    values["ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8"                                ] = lumi *     0.04837 / 1.86044725000000000e+06;
     values["DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"                  ] = lumi *      6025.2 / 9.04033200000000000e+06;
     values["WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"                       ] = lumi * 60290.*1.23 / 7.16990320000000000e+07;
     values["WW_TuneCUETP8M1_13TeV-pythia8"                                           ] = lumi *       118.7 / 9.92818187500000000e+05;
@@ -24,8 +24,11 @@ std::map<std::string, float> GetLumis() {
     values["ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1"          ] = lumi *       38.09 / 9.01010125000000000e+05;
     values["ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1"      ] = lumi *       38.09 / 9.06457375000000000e+05;
 
-    values["SingleMuon" ] = 1.000000;
-    values["SingleMuon-v2" ] = 1.000000;
+    values["SingleMuon" ]     = 1.000000;
+    values["SingleElectron" ] = 1.000000;
+    values["DoubleMuon" ]     = 1.000000;
+    values["DoubleEG" ]       = 1.000000;
+    values["MET" ]            = 1.000000;
 
     return values;
 }
